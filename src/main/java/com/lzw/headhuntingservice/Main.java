ff158b5d40f120be7806a7ab38fed6b294c4600e
Saika-m <165717332+saika-m@users.noreply.github.com>
2024-04-24 01:58:52 -0400

    Create Main.java

---
+++ src/main/java/com/lzw/headhuntingservice/Main.java
@@ -0,0 +1,11 @@
+package com.lzw.headhuntingservice;
+
+import com.lzw.headhuntingservice.config.Application;
+import org.springframework.boot.SpringApplication;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
